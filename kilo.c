93c0e3f3e0d833046d89317d4ead0be392d1880d
notslok <53101134+notslok@users.noreply.github.com>
2022-11-09 14:49:32 +0530

    pure raw mode config done

--- kilo.c
+++ kilo.c
@@ -1,34 +1,72 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <errno.h>
 
+/*** data ***/
 
 struct termios orig_termios;
 
-void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+/*** terminal ***/
+
+void die(const char* s) {
+    perror(s);
+    exit(1);
 }
 
-void enableRawMode(){
+void disableRawMode()
+{
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
+
+void enableRawMode()
+{
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){
+        die("tcgetattr");
+    }
 
     tcgetattr(STDIN_FILENO, &orig_termios);
-    
+
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
 
-    raw.c_lflag &= ~(ECHO | ICANON); 
-    
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | IXON | ICRNL);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
+        die("tcsetattr");
+    };
 }
 
-int main(){
+/*** init ***/
+
+int main()
+{
     enableRawMode();
 
-    char c;
-    
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (1)
+    {
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q')
+            break;
+    };
+
     return 0;
 }
