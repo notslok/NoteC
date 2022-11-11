ccca638c2650a79aaa6212bfbc7c6251ed844fab
notslok <53101134+notslok@users.noreply.github.com>
2022-11-11 22:12:13 +0530

    text buffer added

--- kilo.c
+++ kilo.c
@@ -113,6 +113,19 @@ struct abuf {
 
 #define ABUF_INIT {NULL, 0}
 
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if(new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
 /*** output ***/
 
 void editorDrawRows() {
